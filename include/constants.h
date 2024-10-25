45bdfb7c9e8e50fbe547c68ab07aae8627dd4e07
chathura-de-silva <ac.chathura@gmail.com>
2024-10-25 11:55:23 +0530

    LED Indicators activated

--- include/constants.h
+++ include/constants.h
@@ -7,4 +7,6 @@
 #define MQ8_PIN 34
 #define MAX_RETRIES 10
 #define WAKEUP_INTERVAL 1
+#define LED_RED 23
+#define LED_GREEN 4
 #endif // __CONSTANTS_H__
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 
 void setup()
 {
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN,OUTPUT);
+  digitalWrite(LED_GREEN,HIGH);
   init_error_mechanism();
   status_t status = init_node();
   if (status == ERROR)
@@ -16,6 +19,7 @@ void setup()
   if (status == OKAY)
   {
     Serial.println("Connected to WiFi");
+    digitalWrite(LED_GREEN,LOW);
   }
   else
   {
@@ -26,12 +30,15 @@ void setup()
   status = receive();
 #endif
 #ifdef HELMENT_DEVICE
+  digitalWrite(LED_RED,HIGH);
   status = send();
   if (status == ERROR)
   {
     show_error();
     ESP.restart();
   }
+    delay(1000);
+   digitalWrite(LED_RED,LOW);
   esp_sleep_enable_timer_wakeup(WAKEUP_INTERVAL * 60000000);
   esp_deep_sleep_start();
 #endif
