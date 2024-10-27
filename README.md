b988f234356fb5939a4b8d31966a0404edf88ba4
chathura-de-silva <ac.chathura@gmail.com>
2024-10-27 23:24:42 +0530

    readme updarte

---
+++ README.md
@@ -0,0 +1,3 @@
+![banner](Razorcrest_minershield_repobanner.png)
+
+*   This repository contains the codebase of the project "MinerShield" by Team Razercrest from CSE ICE 21, University of Moratuwa.
--- include/constants.h
+++ include/constants.h
@@ -6,7 +6,7 @@
 #define DHTPIN 32
 #define MQ8_PIN 34
 #define MAX_RETRIES 10
-#define WAKEUP_INTERVAL 1
+#define WAKEUP_INTERVAL 0.25
 #define LED_RED 23
 #define LED_GREEN 4
 #endif // __CONSTANTS_H__
